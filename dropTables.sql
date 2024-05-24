823c40122e4523226b51653191a5b41507e2aa48
HodayaElbazz <116655320+HodayaElbazz@users.noreply.github.com>
2024-05-24 07:27:52 +0300

    Add files via upload

---
+++ dropTables.sql
@@ -0,0 +1,16 @@
+drop table academic_institution
+CASCADE CONSTRAINTS;
+drop table studentcouncil
+CASCADE CONSTRAINTS;
+drop table pupil
+CASCADE CONSTRAINTS;
+drop table representive
+CASCADE CONSTRAINTS;
+drop table training
+CASCADE CONSTRAINTS;
+drop table teacher
+CASCADE CONSTRAINTS;
+drop table participant
+CASCADE CONSTRAINTS;
+drop table teach
+CASCADE CONSTRAINTS;
