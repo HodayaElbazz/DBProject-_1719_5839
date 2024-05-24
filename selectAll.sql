195d9c3716e9f8dcf23ded24c2b38f415297f4f8
HodayaElbazz <116655320+HodayaElbazz@users.noreply.github.com>
2024-05-24 07:28:12 +0300

    Add files via upload

---
+++ selectAll.sql
@@ -0,0 +1,23 @@
+-- לצפות בכל הנתונים בטבלת studentCouncil
+SELECT * FROM studentCouncil;
+
+-- לצפות בכל הנתונים בטבלת academic_institution
+SELECT * FROM academic_institution;
+
+-- לצפות בכל הנתונים בטבלת pupil
+SELECT * FROM pupil;
+
+-- לצפות בכל הנתונים בטבלת representive
+SELECT * FROM representive;
+
+-- לצפות בכל הנתונים בטבלת training
+SELECT * FROM training;
+
+-- לצפות בכל הנתונים בטבלת teacher
+SELECT * FROM teacher;
+
+-- לצפות בכל הנתונים בטבלת participant
+SELECT * FROM participant;
+
+-- לצפות בכל הנתונים בטבלת teach
+SELECT * FROM teach;
